72d7c7ee955bd46fa40bdc6a26c59e0fd0cd1614
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-06-21 23:12:20 +0500

    1

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "dQ5aZhE3fSPw9rxynMPyZMbfixaMuLV4Q868evoQeYY"
-    static let secretKey = "qVtphx3u6MVKUS6bdI7KfJodHiVr0GMusPEtF1C31qw"
+    static let accessKey = "pV6xhy75yGCUaEKTDdxHfmB-6gIZ3RDsj_yB_ntQ3i4"
+    static let secretKey = "LJp0_OtLP0hqsKq760ISI5Xq0dqZlxnb4ZDfW_Do3Rg"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = "https://api.unsplash.com"
